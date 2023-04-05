7a6cd2c3e216e11ef148051c16c5d1c9673ecaba
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-04-05 16:46:31 -0300

    refactor my navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const MyNavBarComponent = () => {
   const [activeMenu, setActiveMenu] = useState<boolean>(false);
+  const [activeMenuHouse, setActiveMenuHouse] = useState<boolean>(false);
+  const [activeMenuAddress, setActiveMenuAddress] = useState<boolean>(false);
+
+  const handleMenuHouse = useCallback(() => {
+    setActiveMenuHouse(!activeMenuHouse);
+  }, [activeMenuHouse, setActiveMenuHouse]);
+
+  const handleMenuAddress = useCallback(() => {
+    setActiveMenuAddress(!activeMenuAddress);
+  }, [activeMenuAddress, setActiveMenuAddress]);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -30,19 +40,67 @@ const MyNavBarComponent = () => {
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
               <Link className="nav-link active" to="/">
-                Home
+                House App
                 <span className="visually-hidden">(current)</span>
               </Link>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+                onMouseEnter={handleMenuHouse}
+                onMouseOut={handleMenuHouse}
+              >
                 House
               </a>
+              <div
+                className={
+                  activeMenuHouse ? 'dropdown-menu show' : 'dropdown-menu'
+                }
+              >
+                <a className="dropdown-item" href="#">
+                  See registered houses
+                </a>
+                <a className="dropdown-item" href="#">
+                  Delete my house
+                </a>
+                <a className="dropdown-item" href="#">
+                  Edit my house
+                </a>
+              </div>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+                onMouseEnter={handleMenuAddress}
+                onMouseOut={handleMenuAddress}
+              >
                 Address
               </a>
+              <div
+                className={
+                  activeMenuAddress ? 'dropdown-menu show' : 'dropdown-menu'
+                }
+              >
+                <a className="dropdown-item" href="#">
+                  See registered addresses
+                </a>
+                <a className="dropdown-item" href="#">
+                  Delete my address
+                </a>
+                <a className="dropdown-item" href="#">
+                  Edit my address
+                </a>
+              </div>
             </li>
             <li className="nav-item">
               <a
